dc2824567bc3e4f85e8860333efde9218828f09e
iCharlieAraiza <eliptikman1234@gmail.com>
2021-08-19 22:05:16 -0500

    Update query.sql

--- DB/query.sql
+++ DB/query.sql
@@ -12,9 +12,7 @@ SELECT persona.nombre, persona.apellido_p, persona.apellido_m, persona.sexo FROM
 	INNER JOIN persona ON profesor.id_persona_fk = persona.id_persona AND profesor.id_departamento_fk IS NULL;
 
 
-
 SELECT COUNT(*) FROM alumno;
-
 
 
 SELECT asignatura.nombre, CONCAT(persona.nombre, ' ' , persona.apellido_p, ' ' , persona.apellido_m) FROM persona
@@ -27,4 +25,11 @@ SELECT asignatura.nombre, CONCAT(persona.nombre, ' ' , persona.apellido_p, ' ' ,
 SELECT persona.nombre, persona.apellido_p, persona.apellido_m, asignatura.nombre FROM persona
 	INNER JOIN profesor ON persona.id_persona = profesor.id_persona_fk
 	INNER JOIN grupo ON grupo.id_profesor_fk = profesor.id_profesor
+	INNER JOIN asignatura ON asignatura.id_asignatura = grupo.id_asignatura_fk;
+	
+	
+SELECT asignatura.nombre, CONCAT(persona.nombre, ' ' , persona.apellido_p, ' ' , persona.apellido_m) FROM persona
+	INNER JOIN alumno ON alumno.id_persona_fk = persona.id_persona
+	INNER JOIN grupo_alumno ON alumno.id_alumno = grupo_alumno.id_alumno_fk
+	INNER JOIN grupo ON grupo.id_grupo = grupo_alumno.id_grupo_fk AND grupo.id_asignatura_fk = 1
 	INNER JOIN asignatura ON asignatura.id_asignatura = grupo.id_asignatura_fk;
